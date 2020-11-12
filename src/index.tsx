4514224b0c9abc36d32b5cf55957570a2ac0a270
yami-beta <yami-beta@users.noreply.github.com>
2020-11-12 13:56:43 +0900

    hotfix: add Counter to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import { Counter } from "./components/Counter";
 
 const App: React.FC<{}> = () => {
   return (
     <div>
       <h1>Hello World</h1>
+
+      <h2>Counter</h2>
+      <Counter />
     </div>
   );
 };
